5a0c159c1d2bb84428f83cd5a37244969da69522
Gustavo <gustavo@gmail.com>
2022-11-04 10:11:21 -0500

    modified arrays principal

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -38,9 +38,130 @@ async function accessSpreadsheet() {
 
     //console.log("cellA1: ",cellA1.value)
     //console.log("cellC3: ",cellC3.formattedValue)
-  
-    //Todos los clientes
-    
+ 
+//Todos los clientes
+
+console.log(sheet.rowCount)
+datanodecol1 = new Array; //columna 1
+datanodecol2 = new Array; //columna 2
+datanodecol3 = new Array; //columna 3
+datanodecol4 = new Array; //columna 4
+datanodecol5 = new Array; //columna 5
+datanodecol6 = new Array; //columna 6
+
+for (let i=0; i<sheet.rowCount; i++)
+{
+        const cellvalue = sheet.getCell(i, 0);
+        //console.log("VALOR: ","i: ",i,"j: ",j, " - ",cellvalue.formattedValue)
+        datanodecol1.push(cellvalue.formattedValue)
+}
+
+for (let i=0; i<sheet.rowCount; i++)
+{
+        const cellvalue = sheet.getCell(i, 1);
+        //console.log("VALOR: ","i: ",i,"j: ",j, " - ",cellvalue.formattedValue)
+        datanodecol2.push(cellvalue.formattedValue)
+}
+
+for (let i=0; i<sheet.rowCount; i++)
+{
+        const cellvalue = sheet.getCell(i, 2);
+        //console.log("VALOR: ","i: ",i,"j: ",j, " - ",cellvalue.formattedValue)
+        datanodecol3.push(cellvalue.formattedValue)
+}
+
+for (let i=0; i<sheet.rowCount; i++)
+{
+        const cellvalue = sheet.getCell(i, 3);
+        //console.log("VALOR: ","i: ",i,"j: ",j, " - ",cellvalue.formattedValue)
+        datanodecol4.push(cellvalue.formattedValue)
+}
+
+for (let i=0; i<sheet.rowCount; i++)
+{
+        const cellvalue = sheet.getCell(i, 4);
+        //console.log("VALOR: ","i: ",i,"j: ",j, " - ",cellvalue.formattedValue)
+        datanodecol5.push(cellvalue.formattedValue)
+}
+
+for (let i=0; i<sheet.rowCount; i++)
+{
+        const cellvalue = sheet.getCell(i, 5);
+        //console.log("VALOR: ","i: ",i,"j: ",j, " - ",cellvalue.formattedValue)
+        datanodecol6.push(cellvalue.formattedValue)
+}
+
+
+//console.log(datanodecol1)
+//console.log(datanodecol2)
+
+
+datafromsheet = new Array; //Creamos un array para almacenar solo los Estados
+
+for(var h=0; h < sheet.rowCount; h++)
+ {
+    datafromsheet[h] = new Array; //Crear 1 array por cada fila
+ }
+
+
+for(var k=0; k < sheet.rowCount; k++)
+ {
+    datafromsheet[k][0] = datanodecol1[k];
+ }
+
+ for(var k=0; k < sheet.rowCount; k++)
+ {
+    datafromsheet[k][1] = datanodecol2[k];
+ }
+
+ for(var k=0; k < sheet.rowCount; k++)
+ {
+    datafromsheet[k][2] = datanodecol3[k];
+ }
+
+ for(var k=0; k < sheet.rowCount; k++)
+ {
+    datafromsheet[k][3] = datanodecol4[k];
+ }
+
+ for(var k=0; k < sheet.rowCount; k++)
+ {
+    datafromsheet[k][4] = datanodecol5[k];
+ }
+
+ for(var k=0; k < sheet.rowCount; k++)
+ {
+    datafromsheet[k][5] = datanodecol6[k];
+ }
+
+
+console.log(datafromsheet)
+//AHORAAAAAAAAAA SSIIIIIIIIIIIIIIIIIII
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Workbook w = Workbook.getWorkbook(inputWorkbook);                      
+       Sheet sheet = w.getSheet(0);
+       nom = sheet.getRows();
+       String[][] SheetArray = new String [2][nom];  
+         // change the first number to the number of columns you want, 
+         // or pick up the number same as you did with rows   
+       Cell cell;           
+        // GETS DATA FROM SHEET AND RUNS THROUGH WHOLE LOOP BELOW FOR EACH REFERENCE            
+        for(int j =0;j<sheet.getRows();j++) // cycles through rows and loads into 2d array
+         { // start 6
+          cell = sheet.getCell(0, j); <- your column number here
+          cellcont = cell.getContents();                        
+          SheetArray[0][j] = cellcont;
+           // repeat the above block for each column you want
+           } // end 6
+
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+    /*
     datanode = new Array;
     datatemp = new Array;
 
@@ -54,13 +175,14 @@ async function accessSpreadsheet() {
             datanode.push(cellvalue.formattedValue)
             datatemp[i] = new Array; //Creamos el array en cada vuelta para almacenar cada uno de los estados
                                                   //en un arrayas bidimensional
-            datatemp[i][0] = datanode[i];
+            datatemp[i][j] = datanode[i];
         }
     }
     console.log(datatemp)
     //console.log(datanode)
     console.log(datanode.length)
     //console.log(sheet.getCell(499, 0).formattedValue)
+    */
     
 
     //Buscar un cliente
@@ -91,7 +213,7 @@ async function accessSpreadsheet() {
             cuotadiaria = sheet.getCell(i, 3).formattedValue
             console.log("CUOTA DIARIA: ",cuotadiaria)
 
-            
+
 
         }
     }
@@ -115,7 +237,9 @@ exports.usersControllers = (req, res) => {
             "ALEX JUNIOR SOPLOPUCO ATO"
         ]*/
         //usersList: [datanode]
-        usersList: [datatemp]
+        //usersList: [datatemp]
+        usersList: [datafromsheet]
+        
         
     })
 }

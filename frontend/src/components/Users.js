2b38429d46602b3e5ad39c879ff3db7d16baad23
Gustavo <gustavo@gmail.com>
2022-11-04 13:05:49 -0500

    ok

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -58,7 +58,7 @@ const Users = () => {
       
       //console.log("YEAH BABY : ",users[0])
       //console.log("YEAH BABY : ",users)
-      console.log("hola")
+      console.log("hola ")
       //dataarraysolution = new Array;
       
       //dataarraysolution = users

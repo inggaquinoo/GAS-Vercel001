f776e33a5172c5e5faa832a6e2f5e543a9ea5226
Gustavo <gustavo@gmail.com>
2022-11-05 11:13:37 -0500

    dqwdqw

--- frontend/src/navigator/StackNavigator.js
+++ frontend/src/navigator/StackNavigator.js
@@ -14,7 +14,7 @@ const StackNavigator = () => {
       <Stack.Navigator  screenOptions={{ headerShown: true } } >
         <Stack.Screen name="Login"  options={{ title: "Sofeen APP" }} component={LogIn} />
         <Stack.Screen name="Logout" component={LogOut} />
-        <Stack.Screen name="DentroDrawer" component={DentroDrawer} />
+        { /* <Stack.Screen name="DentroDrawer" component={DentroDrawer} /> */ }
               
         {/* <Stack.Screen name="BottomTabNavigation" options={{ title: "Hoshii Partners" }} component={BottomTabNavigation} />  */}
         <Stack.Screen name="DrawerNavigation" component={DrawerNavigation} />
